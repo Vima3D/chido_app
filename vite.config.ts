060ae60caa0bf777aacd293362dc66df1981bb98
victor <victor.escobar.3d@gmail.com>
2025-10-03 15:17:13 +0100

    mas cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // ← IMPORTAR EL PLUGIN
 
 export default defineConfig({
-  base: "/chido_app/",
-  plugins: [react()],
+  base: "/chido_app/", // importante para GitHub Pages
+  plugins: [react()], // ahora TypeScript lo reconoce
 });
