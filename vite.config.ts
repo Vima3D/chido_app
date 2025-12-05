89a5b8d5c8052e9a915b91637c526f75b34dbb4e
Victor Escobar <victor.escobar.3d@gmail.com>
2025-12-05 14:37:02 +0000

    Update base path in Vite configuration
    Change base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // important for GitHub Pages
   plugins: [react()],
-});
+  base: '/chido_app/'   // El nombre EXACTO de tu repo entre slashes
+})
